473183c7287811d3ab74bbe5658aebbf649ea347
Slavikkq <yaroslavmanuljak@gmail.com>
2024-04-06 23:23:20 +0300

    react hw 01

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import FriendList from "./components/FriendList";
 import friends from "./friends.json";
 import TransactionHistory from "./components/Transaction";
 import transactions from "./transactions.json";
+import userData from "./userData.json";
 
 export default function App() {
   return (
     <>
-      <Profile />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
