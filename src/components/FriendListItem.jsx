95e21ec2265c53619a14502925ba9d9e493062b0
Slavikkq <yaroslavmanuljak@gmail.com>
2024-04-08 23:27:21 +0300

    react hw 01

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,9 +1,11 @@
+import styles from "./FriendListItem.module.css";
+
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p className={isOnline ? "online" : "offline"}>
+      <p className={isOnline ? styles.statusOnline : styles.statusOffline}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
